108528e56084e3212a41f5f567a92800f90269bb
caishuxing <caishuxing@linggan.com>
2018-06-20 22:11:37 +0800

    添加语言

---
+++ src/main/java/com/cai/framework/bean/LanguageModel.java
@@ -0,0 +1,24 @@
+package com.cai.framework.bean;
+
+
+public class LanguageModel {
+
+    private String name;
+    private String local;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+}
---
+++ src/main/java/com/cai/framework/utils/LanguageLocalUtil.java
@@ -0,0 +1,65 @@
+package com.cai.framework.utils;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
+import android.util.DisplayMetrics;
+
+import com.cai.framework.bean.LanguageModel;
+import com.example.clarence.utillibrary.StringUtils;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * APP内应用语言
+ * Created by davy on 2018/3/1.
+ */
+
+public class LanguageLocalUtil {
+    private static List<LanguageModel> languageModels;
+
+    public static void changeLanguage(Context mContext,String language) {
+        if (!StringUtils.isEmpty(language)) {
+            // 本地语言设置
+            Locale myLocale = new Locale(language);
+            Resources resources = mContext.getResources();
+            Configuration configuration = resources.getConfiguration();
+            DisplayMetrics displayMetrics = resources.getDisplayMetrics();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                configuration.setLocale(myLocale);
+            }else {
+                configuration.locale = myLocale;
+            }
+            resources.updateConfiguration(configuration, displayMetrics);
+        }
+    }
+
+    public static List<LanguageModel> getAllLanguage() {
+        if (languageModels == null) {
+            languageModels = new ArrayList<>();
+            String json = AssetUtil.getStringFromAsset(getContext(), "language.json");
+            try {
+                JSONArray jsonArray = new JSONArray(json);
+                int size = jsonArray.length();
+                for (int i = 0; i < size; i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    LanguageModel model = new LanguageModel();
+                    model.setName(jsonObject.optString("name"));
+                    model.setLocal(jsonObject.optString("local"));
+                    //
+                    languageModels.add(model);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+        return languageModels;
+    }
+}
