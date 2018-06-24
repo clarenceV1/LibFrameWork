709a46c45b833b6e35f3a093017b2106f68d6d1e
caishuxing <caishuxing@linggan.com>
2018-06-24 10:42:04 +0800

    添加 spinner的popwindow

---
+++ src/main/java/com/cai/framework/widget/spiner/SpinerPopWindow.java
@@ -0,0 +1,78 @@
+package com.cai.framework.widget.spiner;
+
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.PopupWindow;
+
+import com.cai.framework.R;
+
+public class SpinerPopWindow extends PopupWindow {
+
+    private Context mContext;
+    private ListView mListView;
+    private BaseAdapter mAdapter;
+
+    public SpinerPopWindow(Context context) {
+        super(context);
+        mContext = context;
+        init();
+    }
+
+    public void setOnItemClickListener(AdapterView.OnItemClickListener onItemClickListener) {
+        if (mListView != null) {
+            mListView.setOnItemClickListener(onItemClickListener);
+        }
+    }
+
+    public void setAdatper(BaseAdapter adapter) {
+        mAdapter = adapter;
+        if (mListView != null) {
+            mListView.setAdapter(mAdapter);
+        }
+    }
+
+
+    private void init() {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.spiner_pop_window, null);
+        setContentView(view);
+        setWidth(ViewGroup.LayoutParams.WRAP_CONTENT);
+        setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        /**
+         * 设置背景只有设置了这个才可以点击外边和BACK消失
+         */
+        setFocusable(true);
+        ColorDrawable dw = new ColorDrawable(0x00);
+        setBackgroundDrawable(dw);
+        /**
+         *设置可以触摸
+         */
+        setTouchable(true);
+        /**
+         * 设置点击外边可以消失
+         */
+        setOutsideTouchable(true);
+        setTouchInterceptor(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                /**
+                 * 判断是不是点击了外部
+                 */
+                if (event.getAction() == MotionEvent.ACTION_OUTSIDE) {
+                    return true;
+                }
+                //不是点击外部
+                return false;
+            }
+        });
+        mListView = (ListView) view.findViewById(R.id.listview);
+    }
+}
