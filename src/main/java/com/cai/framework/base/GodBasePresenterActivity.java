d5d1ceb1ea0f5d425a83dcf828d91bff5716d492
caishuxing <caishuxing@linggan.com>
2018-06-26 16:01:30 +0800

    添加沉浸式状态栏

--- src/main/java/com/cai/framework/base/GodBasePresenterActivity.java
+++ src/main/java/com/cai/framework/base/GodBasePresenterActivity.java
@@ -1,14 +1,18 @@
 package com.cai.framework.base;
 
 
+import android.annotation.TargetApi;
 import android.arch.lifecycle.LifecycleRegistry;
 import android.arch.lifecycle.LifecycleRegistryOwner;
 import android.content.pm.ActivityInfo;
 import android.databinding.ViewDataBinding;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
-import com.cai.framework.utils.stausbar.StatusBarController;
+import com.cai.framework.R;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,16 +24,34 @@ import java.util.List;
 public abstract class GodBasePresenterActivity<M extends ViewDataBinding> extends DataBindingActivity<M> implements LifecycleRegistryOwner {
     private final LifecycleRegistry mRegistry = new LifecycleRegistry(this);
     private List<GodBasePresenter> observerList = new ArrayList<>();
+    public SystemBarTintManager tintManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        StatusBarController.getInstance().setStatusTextColor(this, true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            setTranslucentStatus(true);
+            tintManager = new SystemBarTintManager(this);
+            tintManager.setStatusBarTintEnabled(true);
+            tintManager.setTintColor(getResources().getColor(R.color.black_a));
+        }
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         initDagger();
         initPresenter();
         initView();
+    }
+
+    @TargetApi(19)
+    private void setTranslucentStatus(boolean on) {
+        Window win = getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
     }
 
     private void initPresenter() {
