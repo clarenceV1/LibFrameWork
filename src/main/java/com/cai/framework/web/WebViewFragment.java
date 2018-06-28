9ee1566ff28d82993794bf203bd6879e05b41544
caishuxing <caishuxing@linggan.com>
2018-06-28 19:05:21 +0800

    添加判断

--- src/main/java/com/cai/framework/web/WebViewFragment.java
+++ src/main/java/com/cai/framework/web/WebViewFragment.java
@@ -73,12 +73,16 @@ public class WebViewFragment extends GodBasePresenterFragment<WebVewFragmentBind
 
         //设置自适应屏幕，两者合用
         webSettings.setUseWideViewPort(true); //将图片调整到适合webview的大小
-        webSettings.setLoadWithOverviewMode(true); // 缩放至屏幕的大小
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR_MR1) {
+            webSettings.setLoadWithOverviewMode(true); // 缩放至屏幕的大小
+        }
 
         //缩放操作
         //webSettings.setSupportZoom(true); //支持缩放，默认为true。是下面那个的前提。
         //webSettings.setBuiltInZoomControls(true); //设置内置的缩放控件。若为false，则该WebView不可缩放
-        webSettings.setDisplayZoomControls(false); //隐藏原生的缩放控件
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            webSettings.setDisplayZoomControls(false); //隐藏原生的缩放控件
+        }
 
         //缓存模式如下：
         //LOAD_CACHE_ONLY: 不使用网络，只读取本地缓存数据
@@ -93,7 +97,9 @@ public class WebViewFragment extends GodBasePresenterFragment<WebVewFragmentBind
         // webSettings.setAppCachePath(cacheDirPath);
 
         //其他细节操作
-        webSettings.setAllowFileAccess(true); //设置可以访问文件
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.CUPCAKE) {
+            webSettings.setAllowFileAccess(true); //设置可以访问文件
+        }
         webSettings.setJavaScriptCanOpenWindowsAutomatically(false); //支持通过JS打开新窗口
         webSettings.setLoadsImagesAutomatically(true); //支持自动加载图片
         webSettings.setDefaultTextEncodingName("utf-8");//设置编码格式
