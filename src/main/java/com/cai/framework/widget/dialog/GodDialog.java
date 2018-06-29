203c9c326df480ef54d1de43bfb3445ad1769aea
caishuxing <caishuxing@linggan.com>
2018-06-29 20:19:59 +0800

    修改UI

--- src/main/java/com/cai/framework/widget/dialog/GodDialog.java
+++ src/main/java/com/cai/framework/widget/dialog/GodDialog.java
@@ -3,7 +3,6 @@ package com.cai.framework.widget.dialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,14 +11,65 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.cai.framework.R;
+import com.example.clarence.utillibrary.DeviceUtils;
 
 public class GodDialog extends Dialog {
-    public GodDialog(@NonNull Context context, int themeResId) {
-        super(context, themeResId);
+
+    TextView positiveButton;
+    TextView negativeButton;
+    TextView oneButton;
+    private DialogInterface.OnClickListener positiveButtonClickListener;
+    private DialogInterface.OnClickListener negativeButtonClickListener;
+
+    private GodDialog(Builder builder) {
+        super(builder.context,R.style.Dialog);
+        this.oneButton = builder.oneButton;
+        this.positiveButton = builder.positiveButton;
+        this.negativeButton = builder.negativeButton;
+        this.positiveButtonClickListener = builder.positiveButtonClickListener;
+        this.negativeButtonClickListener = builder.negativeButtonClickListener;
+        setCanceledOnTouchOutside(false);
+        setCancelable(false);
+        setClickListener();
     }
 
-    public GodDialog(Context context) {
-        super(context);
+    private void setClickListener() {
+        if (positiveButtonClickListener != null && positiveButton != null) {
+            positiveButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    positiveButtonClickListener.onClick(GodDialog.this, DialogInterface.BUTTON_POSITIVE);
+                    GodDialog.this.dismiss();
+                }
+            });
+        }
+
+        if (negativeButtonClickListener != null && negativeButton != null) {
+            negativeButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    negativeButtonClickListener.onClick(GodDialog.this, DialogInterface.BUTTON_NEGATIVE);
+                    GodDialog.this.dismiss();
+                }
+            });
+        }
+        if (positiveButtonClickListener != null && oneButton != null) {
+            oneButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    positiveButtonClickListener.onClick(GodDialog.this, DialogInterface.BUTTON_POSITIVE);
+                    GodDialog.this.dismiss();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void show() {
+        super.show();
+//        WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
+//        layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+//        //如果需要状态栏透明，就使用下面的方法
+//        layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+//        getWindow().getDecorView().setPadding(0, 0, 0, 0);
+//        getWindow().setAttributes(layoutParams);
     }
 
     public static class Builder {
@@ -28,12 +78,20 @@ public class GodDialog extends Dialog {
         private String message;
         private String positiveButtonText;
         private String negativeButtonText;
-        private View contentView;
         private DialogInterface.OnClickListener positiveButtonClickListener;
         private DialogInterface.OnClickListener negativeButtonClickListener;
 
+        TextView positiveButton;
+        TextView oneButton;
+        TextView negativeButton;
+        View contentView;
+        boolean showOneButton;
+        int screenWidth;
+
+
         public Builder(Context context) {
             this.context = context;
+            screenWidth = DeviceUtils.getScreenWidth(context);
         }
 
         public Builder setMessage(String message) {
@@ -98,6 +156,20 @@ public class GodDialog extends Dialog {
             return this;
         }
 
+        public Builder setOneButton(String positiveButtonText, DialogInterface.OnClickListener listener) {
+            this.positiveButtonText = positiveButtonText;
+            this.positiveButtonClickListener = listener;
+            this.showOneButton = true;
+            return this;
+        }
+
+        public Builder setOneButton(int positiveButtonText, DialogInterface.OnClickListener listener) {
+            this.positiveButtonText = (String) context.getText(positiveButtonText);
+            this.positiveButtonClickListener = listener;
+            this.showOneButton = true;
+            return this;
+        }
+
         public Builder setNegativeButton(int negativeButtonText, DialogInterface.OnClickListener listener) {
             this.negativeButtonText = (String) context.getText(negativeButtonText);
             this.negativeButtonClickListener = listener;
@@ -110,14 +182,12 @@ public class GodDialog extends Dialog {
             return this;
         }
 
-        public GodDialog create() {
+        public GodDialog build() {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             View layout = inflater.inflate(R.layout.god_dialog, null);
-            final GodDialog dialog = new GodDialog(context, R.style.Dialog);
-            dialog.addContentView(layout, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
 
             // set the dialog title
-            TextView tvTitile = (TextView) layout.findViewById(R.id.dialog_title);
+            TextView tvTitile = layout.findViewById(R.id.dialog_title);
             if (TextUtils.isEmpty(title)) {
                 tvTitile.setVisibility(View.GONE);
             } else {
@@ -126,51 +196,49 @@ public class GodDialog extends Dialog {
             }
 
             // set the content message
-            TextView tvMessage = (TextView) layout.findViewById(R.id.dialog_message);
+            TextView tvMessage = layout.findViewById(R.id.dialog_message);
             if (contentView != null) {
-                LinearLayout dialogContentView = (LinearLayout) layout.findViewById(R.id.dialog_content);
+                LinearLayout dialogContentView = layout.findViewById(R.id.dialog_content);
                 dialogContentView.removeAllViews();
                 dialogContentView.addView(contentView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
             } else if (!TextUtils.isEmpty(message)) {
                 tvMessage.setText(message);
-            }else{
+            } else {
                 tvMessage.setVisibility(View.GONE);
             }
-            dialog.setContentView(layout);
-
             // set the confirm button
-            TextView positiveButton = (TextView) layout.findViewById(R.id.btn_dialog_ok);
+            positiveButton = layout.findViewById(R.id.btn_dialog_ok);
             if (TextUtils.isEmpty(positiveButtonText)) {
                 positiveButton.setVisibility(View.GONE);
             } else {
                 positiveButton.setText(positiveButtonText);
-                if (positiveButtonClickListener != null) {
-                    positiveButton.setOnClickListener(new View.OnClickListener() {
-                        public void onClick(View v) {
-                            positiveButtonClickListener.onClick(dialog, DialogInterface.BUTTON_POSITIVE);
-                            dialog.dismiss();
-                        }
-                    });
-                }
-
             }
 
             // set the cancel button
-            TextView negativeButton = (TextView) layout.findViewById(R.id.btn_dialog_cancel);
+            negativeButton = layout.findViewById(R.id.btn_dialog_cancel);
             if (TextUtils.isEmpty(negativeButtonText)) {
                 negativeButton.setVisibility(View.GONE);
             } else {
                 negativeButton.setText(negativeButtonText);
-                if (negativeButtonClickListener != null) {
-                    negativeButton.setOnClickListener(new View.OnClickListener() {
-                        public void onClick(View v) {
-                            negativeButtonClickListener.onClick(dialog, DialogInterface.BUTTON_NEGATIVE);
-                            dialog.dismiss();
-                        }
-                    });
+            }
+
+            oneButton = layout.findViewById(R.id.btn_dialog_one);
+            if (showOneButton) {
+                if (TextUtils.isEmpty(positiveButtonText)) {
+                    oneButton.setVisibility(View.GONE);
+                } else {
+                    oneButton.setText(positiveButtonText);
+                    oneButton.setVisibility(View.VISIBLE);
+                    negativeButton.setVisibility(View.GONE);
+                    positiveButton.setVisibility(View.GONE);
                 }
             }
 
+            GodDialog dialog = new GodDialog(this);
+//            dialog.addContentView(layout, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(DeviceUtils.getScreenWidth(context), DeviceUtils.getScreenHeight(context));
+            dialog.setContentView(layout, layoutParams);
+//            layout.requestLayout();
             return dialog;
         }
     }
