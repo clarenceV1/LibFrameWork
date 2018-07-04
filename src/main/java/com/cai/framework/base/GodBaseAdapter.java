8f5fb48302ec76d72c43b2af47327c96fdac8e70
caishuxing <caishuxing@linggan.com>
2018-07-04 23:09:47 +0800

    去掉没用的

--- src/main/java/com/cai/framework/base/GodBaseAdapter.java
+++ src/main/java/com/cai/framework/base/GodBaseAdapter.java
@@ -6,9 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
-import com.cai.framework.R;
 import com.cai.framework.bean.CBaseData;
-import com.cai.framework.utils.ViewHolder;
 
 import java.util.List;
 
