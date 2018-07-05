756268da0d419886452a41d65d4a0a8ff4231573
caishuxing <caishuxing@linggan.com>
2018-07-05 17:15:23 +0800

    加

--- src/main/java/com/cai/framework/utils/PermissionUtils.java
+++ src/main/java/com/cai/framework/utils/PermissionUtils.java
@@ -115,7 +115,12 @@ public class PermissionUtils {
         new AlertDialog.Builder(context)
                 .setTitle("提示信息")
                 .setMessage("当前应用缺少必要权限，该功能暂时无法使用。如若需要，请单击【确定】按钮前往设置中心进行权限授权。")
-                .setNegativeButton("取消", null)
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
                 .setPositiveButton("确定", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
