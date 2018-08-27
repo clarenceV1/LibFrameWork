aa1a6a6a8fb517ad1030ff50861d7e313651285f
caishuxing <caishuxing@linggan.com>
2018-08-27 10:19:10 +0800

    添加圆角定制化

--- src/main/java/com/cai/framework/imageload/GlideRoundTransform.java
+++ src/main/java/com/cai/framework/imageload/GlideRoundTransform.java
@@ -17,9 +17,21 @@ import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
 public class GlideRoundTransform extends BitmapTransformation {
 
     private static float radius = 0f;
+    boolean leftTop = true;
+    boolean rightTop = true;
+    boolean leftBottom = true;
+    boolean rightBottom = true;
 
     public GlideRoundTransform(Context context) {
         this(context, 4);
+    }
+
+    public GlideRoundTransform(Context context, int corner, boolean leftTop, boolean rightTop, boolean leftBottom, boolean rightBottom) {
+        this(context, corner);
+        this.leftTop = leftTop;
+        this.leftBottom = leftBottom;
+        this.rightBottom = rightBottom;
+        this.rightTop = rightTop;
     }
 
     public GlideRoundTransform(Context context, int dp) {
@@ -29,10 +41,10 @@ public class GlideRoundTransform extends BitmapTransformation {
 
     @Override
     protected Bitmap transform(BitmapPool pool, Bitmap toTransform, int outWidth, int outHeight) {
-        return roundCrop(pool, toTransform);
+        return roundCrop(pool, toTransform, leftTop, rightTop, leftBottom, rightBottom);
     }
 
-    private static Bitmap roundCrop(BitmapPool pool, Bitmap source) {
+    private static Bitmap roundCrop(BitmapPool pool, Bitmap source, boolean leftTop, boolean rightTop, boolean leftBottom, boolean rightBottom) {
         if (source == null) return null;
 
         Bitmap result = pool.get(source.getWidth(), source.getHeight(), Bitmap.Config.ARGB_8888);
@@ -46,6 +58,19 @@ public class GlideRoundTransform extends BitmapTransformation {
         paint.setAntiAlias(true);
         RectF rectF = new RectF(0f, 0f, source.getWidth(), source.getHeight());
         canvas.drawRoundRect(rectF, radius, radius, paint);
+
+        if (!leftTop) {
+            canvas.drawRect(0, 0, radius, radius, paint);
+        }
+        if (!rightTop) {
+            canvas.drawRect(rectF.right - radius, 0, rectF.right, radius, paint);
+        }
+        if (!leftBottom) {
+            canvas.drawRect(0, rectF.bottom - radius, radius, rectF.bottom, paint);
+        }
+        if (!rightBottom) {
+            canvas.drawRect(rectF.right - radius, rectF.bottom - radius, rectF.right, rectF.bottom, paint);
+        }
         return result;
     }
 
