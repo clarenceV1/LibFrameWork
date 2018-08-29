27fdf095a1735b4d5d9ab75683a2654401ffe3ed
caishuxing <caishuxing@linggan.com>
2018-08-29 10:12:33 +0800

    添加功能

--- src/main/java/com/cai/framework/widget/ListViewEx.java
+++ src/main/java/com/cai/framework/widget/ListViewEx.java
@@ -5,6 +5,8 @@ import android.util.AttributeSet;
 import android.widget.ListView;
 
 public class ListViewEx extends ListView {
+    private boolean mIsOnMeasure;
+
     public ListViewEx(Context context) {
         super(context);
     }
@@ -14,10 +16,21 @@ public class ListViewEx extends ListView {
     }
 
     @Override
-    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        // TODO Auto-generated method stub
+        mIsOnMeasure = true;
         int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
                 MeasureSpec.AT_MOST);
         super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        mIsOnMeasure = false;
+        super.onLayout(changed, l, t, r, b);
+    }
+
+    public boolean isOnMeasure(){
+        return mIsOnMeasure;
     }
 }
