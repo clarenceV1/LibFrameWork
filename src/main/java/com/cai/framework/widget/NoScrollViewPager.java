5de6f24e3e564030ae8bd329051d025ee74a158f
gakes <491122011@qq.com>
2018-09-03 19:38:12 +0800

    禁止滑动的vierpager

---
+++ src/main/java/com/cai/framework/widget/NoScrollViewPager.java
@@ -0,0 +1,31 @@
+package com.cai.framework.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Description:禁止滑动的viewpager
+ * Created on 2017-7-26
+ */
+public class NoScrollViewPager extends ViewPager {
+
+    public NoScrollViewPager(Context context) {
+        super(context);
+    }
+
+    public NoScrollViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return false;
+    }
+}
