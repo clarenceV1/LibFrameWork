b63c8c319e464e9c13cf0587d911be0ff137342c
gakes <491122011@qq.com>
2018-09-04 20:58:49 +0800

    返回键的更换

--- src/main/java/com/cai/framework/base/GodBaseApplication.java
+++ src/main/java/com/cai/framework/base/GodBaseApplication.java
@@ -93,7 +93,7 @@ public abstract class GodBaseApplication extends Application {
                 .methodCount(0)         // (Optional) How many method line to show. Default 2
                 .methodOffset(7)        // (Optional) Hides internal method calls up to offset. Default 5
 //                .logStrategy(customLog) // (Optional) Changes the log strategy to print out. Default LogCat
-                .tag("MyLog")   // (Optional) Global tag for every log. Default PRETTY_LOGGER
+                .tag("logs")   // (Optional) Global tag for every log. Default PRETTY_LOGGER
                 .build();
         AndroidLogAdapter androidLogAdapter = new AndroidLogAdapter(formatStrategy) {
             @Override
