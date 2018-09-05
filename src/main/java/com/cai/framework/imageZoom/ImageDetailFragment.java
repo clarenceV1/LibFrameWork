a023baca31bc82fe1dcf7401744932062e5484a5
caishuxing <caishuxing@linggan.com>
2018-09-05 19:15:40 +0800

    添加图片预览页面

---
+++ src/main/java/com/cai/framework/imageZoom/ImageDetailFragment.java
@@ -0,0 +1,88 @@
+package com.cai.framework.imageZoom;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.cai.framework.R;
+import com.cai.framework.imageZoom.widget.PhotoViewAttacher;
+import com.example.clarence.utillibrary.DeviceUtils;
+
+public class ImageDetailFragment extends Fragment {
+    private String mImageUrl;
+
+    private ImageView mImageView;
+
+    private ProgressBar progressBar;
+
+    private PhotoViewAttacher mAttacher;
+    private SimpleTarget simpleTarget = null;
+
+    public static ImageDetailFragment newInstance(String imageUrl) {
+        ImageDetailFragment f = new ImageDetailFragment();
+        Bundle args = new Bundle();
+        args.putString("url", imageUrl);
+        f.setArguments(args);
+        return f;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mImageUrl = getArguments() != null ? getArguments().getString("url") : null;
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View v = inflater.inflate(R.layout.fragment_image_detail, container, false);
+        mImageView = v.findViewById(R.id.image);
+        mAttacher = new PhotoViewAttacher(mImageView);
+        //单击退出页面
+        mAttacher.setOnPhotoTapListener(new PhotoViewAttacher.OnPhotoTapListener() {
+
+            @Override
+            public void onPhotoTap(View arg0, float arg1, float arg2) {
+                getActivity().finish();
+            }
+        });
+        progressBar = v.findViewById(R.id.loading);
+        loadImage();
+        return v;
+    }
+
+    private void loadImage() {
+           mImageUrl ="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1536152402935&di=42227108a8332874c2c26652a7f3abe8&imgtype=0&src=http%3A%2F%2Fwww.beihaiting.com%2Fuploads%2Fallimg%2F170312%2F10723-1F3121JK4360.jpg";
+        simpleTarget = new SimpleTarget<Bitmap>(SimpleTarget.SIZE_ORIGINAL, SimpleTarget.SIZE_ORIGINAL) {
+            @Override
+            public void onLoadStarted(Drawable placeholder) {
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onResourceReady(Bitmap loadedImage, GlideAnimation<? super Bitmap> glideAnimation) {
+                progressBar.setVisibility(View.GONE);
+                mImageView.setImageBitmap(loadedImage);
+                mAttacher.update();
+            }
+
+            @Override
+            public void onLoadFailed(Exception e, Drawable errorDrawable) {
+                Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.GONE);
+            }
+        };
+        Glide.with(this).load(mImageUrl).asBitmap().into(simpleTarget);
+    }
+
+}
